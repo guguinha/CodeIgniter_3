866f751eeb7ae8ecd0425326d27c01d237324115
Augusto Guimarães <guguinha7@gmail.com>
2019-03-27 23:12:18 -0300

    Ajustes Api Paypal primeiros passos

---
+++ application/controllers/admin/setExpressCheckout.php
@@ -0,0 +1,93 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class SetExpressCheckout extends CI_Controller {
+
+    public function __construct(){
+        parent::__construct();
+
+    }
+
+    public function run(){
+//Incluindo o arquivo que contém a função sendNvpRequest
+//require 'sendNvpRequest.php';
+  $this->load->model('sendNvpRequest_model');
+//Vai usar o Sandbox, ou produção?
+$sandbox = true;
+  
+//Baseado no ambiente, sandbox ou produção, definimos as credenciais
+//e URLs da API.
+if ($sandbox) {
+
+    //credenciais da API para o Sandbox
+
+    $user = 'guguguim-facilitator_api1.hotmail.com';
+
+    $pswd = 'V5P2WRJ7CLFJ3L73';
+
+    $signature = 'AUT0HHlrYTHIzwf.mOsUkWTRm6VQAWYm.sWFWK5Qwf7soSywa9mNL6F2';
+  
+    //URL da PayPal para redirecionamento, não deve ser modificada
+    $paypalURL = 'https://www.sandbox.paypal.com/cgi-bin/webscr';
+} else {
+    //credenciais da API para produção
+    $user = 'usuario';
+    $pswd = 'senha';
+    $signature = 'assinatura';
+  
+    //URL da PayPal para redirecionamento, não deve ser modificada
+    $paypalURL = 'https://www.paypal.com/cgi-bin/webscr';
+}
+  
+//Campos da requisição da operação SetExpressCheckout, como ilustrado acima.
+$requestNvp = array(
+    'USER' => $user,
+    'PWD' => $pswd,
+    'SIGNATURE' => $signature,
+  
+    'VERSION' => '108.0',
+    'METHOD'=> 'SetExpressCheckout',
+  
+    'PAYMENTREQUEST_0_PAYMENTACTION' => 'SALE',
+    'PAYMENTREQUEST_0_AMT' => '22.00',
+    'PAYMENTREQUEST_0_CURRENCYCODE' => 'BRL',
+    'PAYMENTREQUEST_0_ITEMAMT' => '22.00',
+    'PAYMENTREQUEST_0_INVNUM' => '1234', //numero do pedido para identificação
+  
+    'L_PAYMENTREQUEST_0_NAME0' => 'Item A',
+    'L_PAYMENTREQUEST_0_DESC0' => 'Produto A – 110V',
+    'L_PAYMENTREQUEST_0_AMT0' => '11.00',
+    'L_PAYMENTREQUEST_0_QTY0' => '1',
+    'L_PAYMENTREQUEST_0_ITEMAMT' => '11.00',
+    'L_PAYMENTREQUEST_0_NAME1' => 'Item B',
+    'L_PAYMENTREQUEST_0_DESC1' => 'Produto B – 220V',
+    'L_PAYMENTREQUEST_0_AMT1' => '11.00',
+    'L_PAYMENTREQUEST_0_QTY1' => '1',
+  
+    'RETURNURL' => 'http://PayPalPartner.com.br/VendeFrete?return=1',
+    'CANCELURL' => 'http://PayPalPartner.com.br/CancelaFrete',
+    'BUTTONSOURCE' => 'BR_EC_EMPRESA',
+    
+);
+  
+//Envia a requisição e obtém a resposta da PayPal
+$responseNvp = $this->sendNvpRequest_model->sendNvpRequest($requestNvp, $sandbox);
+  
+//Se a operação tiver sido bem sucedida, redirecionamos o cliente para o
+//ambiente de pagamento.
+if (isset($responseNvp['ACK']) && $responseNvp['ACK'] == 'Success') {
+    $query = array(
+        'cmd'    => '_express-checkout',
+        'token'  => $responseNvp['TOKEN']
+    );
+
+    $redirectURL = sprintf('%s?%s', $paypalURL, http_build_query($query));
+  
+    header('Location: ' . $redirectURL);
+} else {
+    //Opz, alguma coisa deu errada.
+    //Verifique os logs de erro para depuração.
+}
+
+}
+}
--- application/views/backend/pagamento.php
+++ application/views/backend/pagamento.php
@@ -11,13 +11,13 @@
             <div class="panel panel-default">
                 <div class="panel-heading">
                    <?= $subtitulo ?>
-                </div>
-                <div class="panel-body">
+               </div>
+               <div class="panel-body">
                     <div class="row">
                         <div class="col-lg-12">
                             <h2>Tela de Pagamento via paypal !</h2>
                         </div>
-                        
+
                     </div>
                     <!-- /.row (nested) -->
                 </div>
@@ -29,30 +29,25 @@
     </div>
     <!-- /.row -->
 
-    <div class="col-lg-12">
+    <div class="row">
+        <div class="col-lg-12">
             <div class="panel panel-default">
                 <div class="panel-heading">
                    <?= 'Endpoints API '.$subtitulo ?>
-                </div>
-                <div class="panel-body">
+               </div>
+               <div class="panel-body">
                     <div class="row">
                         <div class="col-lg-12">
-                        <style>
-                            img{
-                                width: 240px;
-                            }
-                        </style>
                             <?php 
-                                $this->table->set_heading("Ambiente","Endpoint","URL de Redirecionamento");
-                                $this->table->add_row("Sandbox","https://api-3t.sandbox.paypal.com/nvp","https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token={TOKEN}");
-                                $this->table->add_row("Produção","https://api-3t.paypal.com/nvp","https://www.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token={TOKEN}");
-                                $this->table->set_template(array(
-                                    'table_open' => '<table class="table table-striped">'
-                                ));
-                                echo $this->table->generate();
+                            $this->table->set_heading("Ambiente","Endpoint","URL de Redirecionamento");
+                            $this->table->add_row("Sandbox","https://api-3t.sandbox.paypal.com/nvp","https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token={TOKEN}");
+                            $this->table->add_row("Produção","https://api-3t.paypal.com/nvp","https://www.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token={TOKEN}");
+                            $this->table->set_template(array(
+                                'table_open' => '<table class="table table-striped">'
+                            ));
+                            echo $this->table->generate();
                             ?>
                         </div>
-                        
                     </div>
                     <!-- /.row (nested) -->
                 </div>
@@ -60,7 +55,35 @@
             </div>
             <!-- /.panel -->
         </div>
-        <!-- /.col-lg-6 -->
+    </div>
+    <!-- /.row -->
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                   Como colocar em Produção
+                </div>
+
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <style>
+                                img{
+                                    width: 740px;
+                                }
+                            </style>
+                            <img src="<?= base_url().'assets/backend/image/CredenciaisdeProducaoPayPalAPI.png' ?>">
+                        </div>  
+                    </div>
+                    <!-- /.row (nested) -->
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+    </div>
+    <!-- /.row -->
 
 </div>
 <!-- /#page-wrapper -->

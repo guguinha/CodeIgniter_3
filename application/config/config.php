9a761825807aff567b3eb6de3a5b4cef13f1780b
Augusto Guimarães <guguinha7@gmail.com>
2019-04-01 12:27:49 -0300

    Teste envio de email e API de pagamento do paypal

--- application/config/config.php
+++ application/config/config.php
@@ -521,3 +521,14 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+
+$config['smtp_host'] = 'ssl://smtp.gmail.com';
+$config['smtp_port'] = 465;
+$config['smtp_user'] = 'sigamy.email@gmail.com';
+$config['smtp_pass'] = 'gmailgjp2019';
+$config['protocol']  = 'smtp';
+$config['validate']  = TRUE;
+$config['mailtype']  = 'html';
+$config['charset']   = 'utf-8';
+$config['newline']   = "\r\n";
---
+++ application/config/email.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['smtp_host'] = 'ssl://smtp.gmail.com';
+$config['smtp_port'] = 465;
+$config['smtp_user'] = 'sigamy.email@gmail.com';
+$config['smtp_pass'] = 'gmailgjp2019';
+$config['protocol']  = 'smtp';
+$config['validate']  = TRUE;
+$config['mailtype']  = 'html';
+$config['charset']   = 'utf-8';
+$config['newline']   = "\r\n";
